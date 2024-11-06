3519e1bb8cfe5f7969246663bec0fda70e47fe1b
NAVYAPUCHA <puchanavya@gmail.com>
2024-11-06 16:46:54 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,15 @@
+//sample program
+/*
+sample
+java
+program
+*/
+import java.lang.system;
+import java.lang.string;
+class First
+{
+public static void main(string args[])
+{
+system.out.print("WELCOME TO FIRST JAVA PROGRAM");
+}
+}
